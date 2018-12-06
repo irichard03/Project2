03afa33bf50000734b2b8b974b4415a1130c8716
paredfed <paredfed@gmail.com>
2018-12-05 19:43:53 -0600

    my first component

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,18 +3,12 @@
 
 //import Javascript Web Token (JWT)
 // const passport = require("passport-jwt").Stategy, ExtractJwt =require("passport-jwt").ExtractJwt;
+const path = require("path");
 
 module.exports = function(app) {
-    const path = require("path");
     // For anything else, render the html page
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "/../client/build/index.html"));
     });
 
-    //
-    /** router.post('/profile', passport.authenticate('jwt', { session: false }),function(req, res) {
-        res.send(req.user.profile);
-        }
-    );
-    */
 };
