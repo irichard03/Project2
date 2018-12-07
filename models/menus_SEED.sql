57398b4719db8e22f45e04316524fb32a6e61ee6
Ian Richard <ianrichard03@gmail.com>
2018-12-06 23:04:51 -0600

    fixed menu.js and added employee.js in models folder for test and associated seeds.

---
+++ models/menus_SEED.sql
@@ -0,0 +1,31 @@
+USE marios_db;
+
+INSERT INTO menus(dish,dishType,dishCost,dishPrice,alcohol)
+VALUES('Mozzerella Sticks','appetizer',2.00,8.50,0);
+
+INSERT INTO menus(dish,dishType,dishCost,dishPrice,alcohol)
+VALUES('Garlic Knots','appetizer',1.85,6.50,0);
+
+INSERT INTO menus(dish,dishType,dishCost,dishPrice,alcohol)
+VALUES('Basket of French Fries','appetizer',0.75,3.00,0);
+
+INSERT INTO menus(dish,dishType,dishCost,dishPrice,alcohol)
+VALUES('pizza','entree',2.75,9.00,0);
+
+INSERT INTO menus(dish,dishType,dishCost,dishPrice,alcohol)
+VALUES('Calzone','entree',2.00,8.75,0);
+
+INSERT INTO menus(dish,dishType,dishCost,dishPrice,alcohol)
+VALUES('Beer Domestic','drink',1.00, 3.50,1);
+
+INSERT INTO menus(dish,dishType,dishCost,dishPrice,alcohol)
+VALUES('Beer Imported','drink',2.00, 5.50,1);
+
+INSERT INTO menus(dish,dishType,dishCost,dishPrice,alcohol)
+VALUES('Beer Pemium','drink',2.00, 7.50,1);
+
+INSERT INTO menus(dish,dishType,dishCost,dishPrice,alcohol)
+VALUES('Wine','drink',3.00, 9.00,1);
+
+INSERT INTO menus(dish,dishType,dishCost,dishPrice,alcohol)
+VALUES('Soda','drink',0.25, 2.00, 0);
